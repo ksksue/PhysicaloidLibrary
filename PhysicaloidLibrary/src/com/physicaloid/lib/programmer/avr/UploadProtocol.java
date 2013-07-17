bf920326958a96869717d0839aa80c8c589db5a7
Keisuke SUZUKI <ksksue@gmail.com>
2013-07-17 19:30:14 +0900

    Added copyright, license and thanks to avrdude comments

--- PhysicaloidLibrary/src/com/physicaloid/lib/programmer/avr/UploadProtocol.java
+++ PhysicaloidLibrary/src/com/physicaloid/lib/programmer/avr/UploadProtocol.java
@@ -1,3 +1,30 @@
+/*
+ * Copyright (C) 2013 Keisuke SUZUKI
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * Distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * This code has built in knowledge of avrdude.
+ * Thanks to avrdude coders
+ *  Brian S. Dean, Joerg Wunsch, Eric Weddington, Jan-Hinnerk Reichert,
+ *  Alex Shepherd, Martin Thomas, Theodore A. Roth, Michael Holzt
+ *  Colin O'Flynn, Thomas Fischl, David Hoerl, Michal Ludvig,
+ *  Darell Tan, Wolfgang Moser, Ville Voipio, Hannes Weisbach,
+ *  Doug Springer, Brett Hagman
+ *  and all contributers.
+ */
+
 package com.physicaloid.lib.programmer.avr;
 
 import com.physicaloid.lib.Physicaloid.UploadCallBack;
